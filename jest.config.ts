1c677fabee4f358e0762719d2f1e2b616165305f
Christopher Murphy <flyweight@pm.me>
2021-07-19 10:08:46 -0600

    chore(deps): upgrade jest

--- jest.config.ts
+++ jest.config.ts
@@ -137,7 +137,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: "jest-environment-jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
